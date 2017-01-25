3e2a7bea079506ba63f03281e28576de4dd18ce6
Gireesh Babu <gireesh.babu@mozanta.com>
2017-01-25 08:47:19 +0530

    Completed the data two-way binding

--- public/app/stories/story.controller.js
+++ public/app/stories/story.controller.js
@@ -2,8 +2,27 @@ class StoryController {
 
     constructor(){
 
+        this._name = "myName";
+
     }
 
+    set name(name){
+        console.log('name works - '+ name);
+        this._name = name;
+    }
+
+    get name(){
+        //console.log('name works - '+ this._name);
+        return this._name;
+    }
+
+    set main(main){
+        this._main = main;
+    }
+
+    get main(){
+        return this._main;
+    }
 }
 
 StoryController.$inject = [];
